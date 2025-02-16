1a3f400be48490f627a9b6698e398b1310fceafb
John Carlo Petinglay <kharlmostwanted@gmail.com>
2025-02-16 22:43:01 +0800

    added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This is a comment
+git pull
+composer install --optimize-autoloader --no-dev
+php artisan migrate --force
+php artisan route:clear
+php artisan event:clear
+php artisan view:clear
+sudo php artisan cache:clear
+php artisan config:clear
+php artisan up
+php artisan db:seed --force
+
+
